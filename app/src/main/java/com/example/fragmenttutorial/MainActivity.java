000b1cb8b7d877d57e352d66427c6d931caad1ac
mohammad <m.zandieh1999@gmail.com>
2021-07-27 15:18:25 +0430

    completed ViewPage2 And TabLayout

--- app/src/main/java/com/example/fragmenttutorial/MainActivity.java
+++ app/src/main/java/com/example/fragmenttutorial/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fragmenttutorial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -10,35 +12,55 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 import java.util.EventListener;
 
-public class MainActivity extends AppCompatActivity implements MyDialog.MyDialogEventListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.btn_main_showDialog);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager_main);
+        Slideadapter slideadapter = new Slideadapter(this);
+        viewPager2.setAdapter(slideadapter);
+        viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        viewPager2.setCurrentItem(2);
+
+
+        TabLayout tabLayout = findViewById(R.id.tabLayoutMain);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
-            public void onClick(View v) {
-                MyDialog myDialog = new MyDialog();
-                myDialog.setCancelable(false);
-                myDialog.show(getSupportFragmentManager(), null);
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText("A");
+                        break;
+
+                    case 1:
+                        tab.setText("B");
+                        break;
+
+                    case 2:
+                        tab.setText("C");
+                        break;
+
+                    case 3:
+                        tab.setText("D");
+                        break;
+
+                    case 4:
+                        tab.setText("E");
+                        break;
+                }
             }
         });
 
-    }
+        tabLayoutMediator.attach();
 
-    @Override
-    public void onOkButtonClockrd(String data) {
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(data);
-    }
-
-    @Override
-    public void onCancleButtonClicked() {
-        Toast.makeText(this, "Cancle Button Clicked", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/fragmenttutorial/Slideadapter.java
@@ -0,0 +1,52 @@
+package com.example.fragmenttutorial;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class Slideadapter extends FragmentStateAdapter {
+
+    public Slideadapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        String data;
+        switch (position) {
+            case 0:
+                data = "A";
+                break;
+
+            case 1:
+                data = "B";
+                break;
+
+            case 2:
+                data = "C";
+                break;
+
+            case 3:
+                data = "D";
+                break;
+
+            case 4:
+                data = "E";
+                break;
+
+            default:
+                data = "";
+                break;
+
+
+        }
+        return SlideFragment.newInstance(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+}
